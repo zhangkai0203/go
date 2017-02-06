611e89ad283cb8bece9edfd351879a95af6fec82
张凯 <zhangkai020305@163.com>
2017-02-06 10:40:26 +0800

    sumit

---
+++ struct.go
@@ -0,0 +1,97 @@
+package main
+
+import "fmt"
+
+//构造方法
+/*type person struct{
+    name string
+	age  int
+}*/
+
+//嵌套
+/*type person struct{
+    name string
+	age  int
+	contact struct{
+	    phone,city string
+	}
+}*/
+
+//继承
+type xuexiao struct{
+    sex int
+}
+
+type a struct{
+    xuexiao
+    name string
+	age  int
+}
+type b struct{
+    xuexiao
+    name string
+	age  int
+}
+
+func main(){
+    //第一种默认值的方法
+	/*a := person{}
+	a.name = "zhangsan"
+	a.age = 14*/
+	
+	//第二种默认值的方法
+	/*a := person{
+	    name : "zhangsan",
+		age  : 19,
+	}*/
+	
+	//第三种默认值方法
+	/*a := struct{
+        name string
+        age  int		
+	}{
+	    name : "zhangsan",
+		age  : 19,
+	}*/
+	
+	/*a := person{name:"zhangsan",age:19}
+	a.contact.phone = "123"
+	a.contact.city  = "beijing"*/
+	
+	a := a{name:"zhangsan",age:17,xuexiao:xuexiao{sex:0}}
+	b := b{name:"lisi",age:19,xuexiao:xuexiao{sex:1}}
+	
+	fmt.Println(a)
+	fmt.Println(b)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    
+}
