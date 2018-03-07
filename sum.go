c4af042bae5fe888b76a266ea3d96be7534ff783
zhangkai020305@163.com <zhangkai020305@163.com>
2018-03-07 10:05:16 +0800

    更新资料

--- sum.go
+++ sum.go
@@ -2,14 +2,6 @@ package main
 
 import "fmt"
 
-func main(){
-        var a float32 = 10.8
-		var b float32 = 20.6 
-		var c float32 = 0
-		
-		c = sum(a,b)
-		fmt.Println(c)
-}
-func sum(a,b float32)float32{
-    return a+b;
+func main() {
+    fmt.Println("aaaaa");
 }
